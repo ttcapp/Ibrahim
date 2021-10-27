0732eb63fb8b6a15682eb60a25cd87f31d687654
akhtaruzzam <akhtaruzzaman.st@gmail.com>
2021-10-27 12:46:11 +0600

    27/10/21  1st

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/image/ttc.png"),
+            Image.asset("assets/image/ibrahim.jpg"),
+            Image.asset("assets/image/ttc.png"),
+            Image.asset("assets/image/ibrahim1.jpg"),
+            Image.asset("assets/image/ibrahim1.jpg"),
+
+
+
+        ]),
+      ),
+      
+    );
+  }
+}
